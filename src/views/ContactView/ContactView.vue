c09799ab1e54912cf7c255dbb522e5fe183312f5
BuyakovaVA <lineviktoriya@gmail.com>
2023-07-31 01:12:16 +0200

    add clinics info

--- src/views/ContactView/ContactView.vue
+++ src/views/ContactView/ContactView.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
     <h1>Our Clinics</h1>
+    <div v-for="clinic in clinics" :key="clinic.id">
+      <h3>{{ clinic.name }}</h3>
+      <p>Country: {{ clinic.country }}</p>
+      <p>City: {{ clinic.city }}</p>
+      <p>Medical Director: {{ clinic.medicalDirector }}</p>
+      <p>Phone: {{ clinic.phone }}</p>
+    </div>
     <div id="map" style="height: 400px;"></div>
   </div>
 </template>
 
 <script>
+import { clinics } from "@/data/clinicData";
 export default {
   mounted() {
     this.initMap();
+  },
+  data() {
+    return {
+      clinics: clinics,
+    };
   },
   methods: {
     initMap() {
@@ -29,20 +42,6 @@ export default {
           zoom: 3
         });
 
-        const clinics = [
-          { name: "Clinic A", lat: 40.7128, lng: -74.0060, phone: "123-456-7890" },
-          { name: "Clinic A", lat: 48.8566, lng: 2.3522, phone: "123-456-7890" },
-          { name: "Clinic B", lat: 51.5074, lng: -0.1278, phone: "123-456-7890" },
-          { name: "Clinic C", lat: 41.3851, lng: 2.1734, phone: "123-456-7890" },
-          { name: "Clinic D", lat: 52.5200, lng: 13.4050, phone: "123-456-7890" },
-          { name: "Clinic E", lat: 41.9028, lng: 12.4964, phone: "123-456-7890" },
-          { name: "Clinic F", lat: 48.2082, lng: 16.3738, phone: "123-456-7890" },
-          { name: "Clinic G", lat: 52.3667, lng: 4.8945, phone: "123-456-7890" },
-          { name: "Clinic H", lat: 59.3293, lng: 18.0686, phone: "123-456-7890" },
-          { name: "Clinic I", lat: 60.1699, lng: 24.9384, phone: "123-456-7890" },
-          { name: "Clinic J", lat: 52.2297, lng: 21.0122, phone: "123-456-7890" }
-        ];
-
         clinics.forEach((clinic) => {
           const marker = new google.maps.Marker({
             position: { lat: clinic.lat, lng: clinic.lng },
@@ -62,6 +61,7 @@ export default {
     }
   }
 };
+
 </script>
 
 <style>
