417cf284f6316dffdd3f0d2d38b5b892c0ab5979
BuyakovaVA <lineviktoriya@gmail.com>
2023-08-03 01:25:26 +0200

    add styles to contact card

--- src/components/ContactCard/ContactCard.vue
+++ src/components/ContactCard/ContactCard.vue
@@ -1,31 +1,48 @@
 <template>
-  <div>
-    <h3>{{ clinic.name }}</h3>
-    <p>Country: {{ clinic.country }}</p>
-    <p>City: {{ clinic.city }}</p>
-    <p>Medical Director: {{ clinic.medicalDirector }}</p>
-    <p>Phone: {{ clinic.phone }}</p>
+  <div class="contactCard">
+    <h3 class="contactCard--header">{{ clinic.name }}</h3>
+    <div>
+      <p class="contactCard--data">
+        <span class="contactCard--data-name">Country:</span>
+        <span>{{ clinic.country }}</span>
+      </p>
+      <p class="contactCard--data">
+        <span class="contactCard--data-name">City:</span>
+        <span>{{ clinic.city }}</span>
+      </p>
+      <p class="contactCard--data">
+        <span class="contactCard--data-name">Medical Director:</span>
+        <span>{{ clinic.medicalDirector }}</span>
+      </p>
+      <p class="contactCard--data">
+        <span class="contactCard--data-name">Phone:</span>
+        <span>{{ clinic.phone }}</span>
+      </p>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from "vue";
+
+interface Clinic {
+  name: string;
+  country: string;
+  city: string;
+  medicalDirector: string;
+  phone: string;
+}
+
+export default defineComponent({
   props: {
     clinic: {
-      type: {
-        name: String,
-        country: String,
-        city: String,
-        medicalDirector: String,
-        phone: String,
-      },
-      required: true,
-    },
-  },
-
-};
+      type: Object as () => Clinic,
+      required: true
+    }
+  }
+});
 </script>
 
 <style scoped>
-
+@import './ContactCard.css';
 </style>
