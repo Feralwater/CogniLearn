06990328590493250f02301c4536a444142a0cad
BuyakovaVA <lineviktoriya@gmail.com>
2023-10-01 19:17:45 +0200

    add download btn

--- src/views/TestResultsView/TestResultsView.vue
+++ src/views/TestResultsView/TestResultsView.vue
@@ -2,6 +2,13 @@
   <div>
     <h1>Your result: {{ $route.query.result }}</h1>
     <h2>{{ depressionResults }}</h2>
+
+    <v-btn
+      class="downloadBtn"
+      color="primary"
+    >
+      Download PDF
+    </v-btn>
   </div>
 </template>
 
@@ -12,11 +19,18 @@ import { getTestResultDescription } from "@/utils/testResultProcessor";
 export default {
   data() {
     return {
-      depressionResults: "",
+      depressionResults: ""
     };
   },
   created() {
     this.depressionResults = getTestResultDescription(this.$route.query.result);
-  },
+  }
 };
 </script>
+
+<style scoped>
+.downloadBtn {
+  margin: 20px 0 0 20px;
+  cursor: pointer;
+}
+</style>
