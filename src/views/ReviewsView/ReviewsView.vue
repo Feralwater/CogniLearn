6eafeed5b2a3d344dbdb17ec58796c96285287e9
BuyakovaVA <lineviktoriya@gmail.com>
2023-10-10 22:50:16 +0200

    add reviews form

--- src/views/ReviewsView/ReviewsView.vue
+++ src/views/ReviewsView/ReviewsView.vue
@@ -29,15 +29,31 @@ export default {
 
 <template>
   <div>
-    <div v-for="review in reviews" :key="review.id">
-      <div>
-        <h4>{{ review.name }}</h4>
+    <v-form class="review-form">
+      <input type="text" placeholder="Name" class="input-field" />
+      <textarea placeholder="Review" class="input-field"></textarea>
+      <select class="rating-select">
+        <option value="5">5</option>
+        <option value="4">4</option>
+        <option value="3">3</option>
+        <option value="2">2</option>
+        <option value="1">1</option>
+      </select>
+      <div class="button-container">
+        <v-btn color="primary">Add Review</v-btn>
+      </div>
+    </v-form>
+    <v-divider></v-divider>
+    <v-card v-for="review in reviews" :key="review.id" class="mt-4">
+      <v-card-title>{{ review.name }}</v-card-title>
+      <v-card-text>
         <p>{{ review.review }}</p>
         <p>Rating: {{ review.rating }}</p>
-      </div>
-    </div>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <style scoped>
+@import "./ReviewsView.css";
 </style>
