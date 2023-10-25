82376a601ea08ee1eea6e9c20dbee22af8560ef1
BuyakovaVA <lineviktoriya@gmail.com>
2023-10-26 00:07:25 +0200

    add slider

---
+++ src/components/SlideGroup/SlideGroup.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { Doctor } from "@/types/doctor";
+import DoctorCard from "@/components/DoctorCard/DoctorCard.vue";
+
+export default defineComponent({
+  name: "SlideGroup",
+  components: { DoctorCard },
+  props: {
+    sliderItems: {
+      type: Array as () => Doctor[],
+      required: true
+    }
+  },
+  data() {
+    return {
+      model: null
+    };
+  }
+});
+</script>
+
+<template>
+  <v-slide-group
+    v-model="model"
+    class="pa-4"
+    selected-class="bg-success"
+    show-arrows
+  >
+    <v-slide-group-item
+      v-for="doctor in sliderItems"
+      :key="doctor.id"
+      v-slot="{ isSelected, toggle, selectedClass }"
+    >
+      <doctor-card :doctor="doctor" />
+    </v-slide-group-item>
+  </v-slide-group>
+</template>
+
+<style scoped>
+.pa-4 {
+
+}
+</style>
