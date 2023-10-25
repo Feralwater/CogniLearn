32046e88d7d9b1fe5f4a3fee8ef895a7cb126b7b
BuyakovaVA <lineviktoriya@gmail.com>
2023-10-26 00:26:08 +0200

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import AppHeader from "@/components/AppHeader/AppHeader.vue";
+import AppFooter from "@/components/AppFooter/AppFoote.vue";
 </script>
 
 <template>
   <v-app>
     <AppHeader />
     <router-view />
+    <app-footer />
   </v-app>
 </template>
 
---
+++ src/components/AppFooter/AppFoote.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Routes } from "@/router/routes";
+
+export default defineComponent({
+  name: "AppFooter",
+  data() {
+    return {
+      Routes
+    };
+  }
+});
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        <p>
+          We are here to help you.
+          <router-link :to="Routes.Contact">Contact us</router-link>
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  background-color: var(--vt-c-green);
+  padding: 20px 15px;
+  text-align: center;
+  margin: auto 0 0 0;
+}
+
+.footer p {
+  color: var(--vt-c-white);
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+}
+
+.footer a {
+  display: inline-block;
+  color: var(--vt-c-white-soft);
+}
+
+.footer a:hover {
+  color: var(--vt-c-white);
+  transform: scale(1.05);
+}
+
+</style>
