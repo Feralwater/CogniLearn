00f9fcd4489fcff6c24d5614d3411c97c6fb5597
BuyakovaVA <lineviktoriya@gmail.com>
2023-10-26 23:45:51 +0200

    fix login form styles

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,11 @@
-import './assets/main.css'
+import "./assets/main.css";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "@/vuetify";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-// Vuetify
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-  theme: {
-    themes: {
-      light: {
-        colors: {
-          primary: '#14A09D',
-        }
-      }
-    }
-  }
-})
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-app.use(vuetify)
-
-app.mount('#app')
+app.use(createPinia()).use(router).use(vuetify).mount("#app");
