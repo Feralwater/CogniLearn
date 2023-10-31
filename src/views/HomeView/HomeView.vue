90da9af97948d871d925a538bc27e077705d22b5
BuyakovaVA <lineviktoriya@gmail.com>
2023-10-31 20:36:39 +0100

    add responsive styles to banner

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,60 +1,44 @@
+<script setup>
+import banner from "@/assets/images/home/banner.png";
+import { Routes } from "@/router/routes";
+import SlideGroup from "@/components/SlideGroup/SlideGroup.vue";
+import { doctors } from "@/data/doctors";
+</script>
+
+
 <template>
   <div>
     <v-img
       :src="banner"
-      gradient="to right, rgba(255, 255, 255, 1), rgba(0, 0, 0, 0.1)"
       max-height="600px"
       cover
     >
-      <div class="home-banner">
-        <h1 class="home-heading">
-          Welcome to Mind Masters
-        </h1>
-        <p class="subheading mb-6">
-          Our psychology app is designed to help you understand yourself better and gain insights into your mental
-          well-being. Whether you want to learn more about psychology, take quizzes, or find support in times of need,
-          our app has something for everyone.
-        </p>
-        <v-btn
-          class="btn"
-          @click="navigateToDepressionTest"
-          color="primary"
-        >
-          Take Mood Assessment Quiz
-        </v-btn>
-      </div>
+      <v-container fluid>
+        <div class="banner-content">
+          <h1 class="banner-title">
+            Welcome to Mind Masters
+          </h1>
+          <h2 class="banner-subtitle">Your Path to Mental Wellness Starts Here</h2>
+          <p class="subheading mb-6 banner-description">
+            Take the First Step towards Healing with our
+            <router-link :to="Routes.Quiz" class="banner-link">Depression Self-Assessment Test</router-link>
+          </p>
+          <blockquote class="banner-quote">
+            <p class="quote-text">"You're not alone, and we're here for you"</p>
+            <p class="quote-author">- Mind Masters Team</p>
+          </blockquote>
+        </div>
+      </v-container>
     </v-img>
+
     <section>
-      <h2 class="home-heading-2">Our Doctors</h2>
+      <h2>Our Doctors</h2>
       <div class="home-slider">
         <slide-group :sliderItems="doctors" />
       </div>
     </section>
   </div>
 </template>
-
-<script>
-import banner from "@/assets/images/home/banner.png";
-import { Routes } from "@/router/routes";
-import SlideGroup from "@/components/SlideGroup/SlideGroup.vue";
-import { doctors } from "@/data/doctors";
-
-export default {
-  components: { SlideGroup },
-  name: "HomeView",
-  methods: {
-    navigateToDepressionTest() {
-      this.$router.push(Routes.Quiz);
-    }
-  },
-  data() {
-    return {
-      banner,
-      doctors
-    };
-  }
-};
-</script>
 
 <style>
 @import "./HomeView.css";
