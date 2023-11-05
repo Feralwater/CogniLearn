6fc67834e81792d5a842fc4b68336ac098bb0009
BuyakovaVA <lineviktoriya@gmail.com>
2023-11-05 21:18:53 +0100

    add test page styles

---
+++ src/components/AppStepper/AppStepper.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { PropType } from "vue";
+
+const props = defineProps({
+  steps: {
+    type: Array as PropType<{ title: string }[]>,
+    required: true
+  },
+  currentStep: {
+    type: Number,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="stepper">
+    <div v-for="(step, index) in props.steps" :key="index" class="step" :class="{ 'active': index === props.currentStep }">
+      {{ step.title }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.stepper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #f0f0f0;
+  padding: 10px;
+}
+
+.step {
+  flex: 1;
+  text-align: center;
+  padding: 10px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  transition: background-color 0.3s, color 0.3s;
+}
+
+.step.active {
+  background-color: #007bff;
+  color: #fff;
+}
+</style>
--- src/views/DepressionTest/DepressionTestView.vue
+++ src/views/DepressionTest/DepressionTestView.vue
@@ -1,96 +1,106 @@
 <template>
-  <div class="quiz-container">
-    <h1 class="quiz-header">Mood Assessment Quiz</h1>
-    <v-card density="default"
-            width="50vw"
-    >
-      <v-card-title class="question-title">
-        <div>Question {{ currentQuestionIndex + 1 }} of {{ questions.length }}</div>
-        <div> {{ currentQuestion.question }}</div>
-      </v-card-title>
-      <v-card-text>
-        <div class="radio-buttons">
-          <label
-            v-for="(option, optionIndex) in currentQuestion.options"
-            :key="optionIndex"
-            :class="['radio-card', { 'radio-card-selected': currentAnswer === option }]"
-          >
-            <input
-              type="radio"
-              :name="'question-' + currentQuestionIndex"
-              v-model="answers[currentQuestionIndex]"
-              :value="option"
-            />
-            {{ option }}
-          </label>
-        </div>
-      </v-card-text>
-      <v-card-actions>
-        <button @click="prevQuestion" :disabled="currentQuestionIndex === 0" class="prev-button">Previous</button>
-        <button @click="nextQuestion" :disabled="currentQuestionIndex === questions.length - 1 || !currentAnswer"
-                class="next-button">
-          Next
-        </button>
-      </v-card-actions>
-    </v-card>
+  <v-container class="test">
+    <app-stepper :steps="steps" :current-step="currentStep" />
 
-    <v-progress-linear color="primary" :model-value="progress"></v-progress-linear>
+    <div class="quiz-container">
+      <v-card density="default" class="quiz-card">
+        <v-card-title>
+          <div class="question-number">Question {{ currentQuestionIndex + 1 }} of {{ questions.length }}</div>
+          <div class="question"> {{ currentQuestion.question }}</div>
+        </v-card-title>
+        <v-card-text>
+          <div class="radio-buttons">
+            <label
+              v-for="(option, optionIndex) in currentQuestion.options"
+              :key="optionIndex"
+              :class="['radio-card', { 'radio-card-selected': currentAnswer === option }]"
+            >
+              <input
+                type="radio"
+                :name="'question-' + currentQuestionIndex"
+                v-model="answers[currentQuestionIndex]"
+                :value="option"
+              />
+              {{ option }}
+            </label>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <div class="quiz-actions">
+            <v-btn
+              @click="prevQuestion"
+              :disabled="currentQuestionIndex === 0"
+              color="primary"
+            >
+              Previous
+            </v-btn>
+            <v-btn
+              @click="nextQuestion"
+              :disabled="currentQuestionIndex === questions.length - 1 || !currentAnswer"
+              color="primary"
+            >
+              Next
+            </v-btn>
+          </div>
+        </v-card-actions>
+      </v-card>
 
-    <v-btn
-      @click="submitTest" color="primary" class="submit-button"
-      v-if="currentQuestionIndex === questions.length - 1"
-      :disabled="!currentAnswer"
-    >
-      Submit Test
-    </v-btn>
-  </div>
+      <v-progress-linear color="primary" :model-value="progress" />
+
+      <div class="quiz-actions">
+        <v-btn
+          @click="submitTest" color="primary" class="submit-button"
+          v-if="currentQuestionIndex === questions.length - 1"
+          :disabled="!currentAnswer"
+        >
+          Submit Test
+        </v-btn>
+      </div>
+    </div>
+  </v-container>
 </template>
 
-<script>
+<script setup lang="ts">
+import { ref, computed } from "vue";
 import { depressionTestQuestions } from "@/data/depressionQuestions";
 import { getTestResult } from "@/utils/testResultProcessor";
 import { Routes } from "@/router/routes";
+import AppStepper from "@/components/AppStepper/AppStepper.vue";
+import router from "@/router";
 
-export default {
-  data() {
-    return {
-      questions: depressionTestQuestions,
-      currentQuestionIndex: 0,
-      answers: Array(depressionTestQuestions.length)
-    };
-  },
-  computed: {
-    currentQuestion() {
-      return this.questions[this.currentQuestionIndex];
-    },
-    progress() {
-      return ((this.currentQuestionIndex + 1) / this.questions.length) * 100;
-    },
-    currentAnswer() {
-      return this.answers[this.currentQuestionIndex];
-    }
-  },
-  methods: {
-    nextQuestion() {
-      if (this.currentQuestionIndex < this.questions.length - 1) {
-        this.currentQuestionIndex++;
-      }
-    },
-    prevQuestion() {
-      if (this.currentQuestionIndex > 0) {
-        this.currentQuestionIndex--;
-      }
-    },
-    submitTest() {
-      const result = getTestResult(this.answers);
-      this.$router.push({
-        path: Routes.Results,
-        query: { result }
-      });
-    }
+const steps = [
+  { title: "Take Test" },
+  { title: "Fill Details" },
+  { title: "Get Results" }
+];
+const currentStep = ref(0);
+const questions = ref(depressionTestQuestions);
+const currentQuestionIndex = ref(0);
+const answers = ref(Array(depressionTestQuestions.length));
+
+const currentQuestion = computed(() => questions.value[currentQuestionIndex.value]);
+const progress = computed(() => ((currentQuestionIndex.value + 1) / questions.value.length) * 100);
+const currentAnswer = computed(() => answers.value[currentQuestionIndex.value]);
+
+const nextQuestion = () => {
+  if (currentQuestionIndex.value < questions.value.length - 1) {
+    currentQuestionIndex.value++;
   }
 };
 
+const prevQuestion = () => {
+  if (currentQuestionIndex.value > 0) {
+    currentQuestionIndex.value--;
+  }
+};
+
+const submitTest = () => {
+  const result = getTestResult(answers.value);
+  router.push({
+    path: Routes.Results,
+    query: { result }
+  });
+};
 </script>
 
 <style>
