4e882f7c41b136712a5c874ee28ce35eb1de1a27
BuyakovaVA <lineviktoriya@gmail.com>
2023-11-24 16:52:46 +0100

    add user icon

--- src/components/AppHeader/AppHeader.vue
+++ src/components/AppHeader/AppHeader.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import logo from "@/assets/icons/logo.svg";
 import mapPointer from "@/assets/icons/Map-Pointer.svg";
-import clock from "@/assets/icons/clock.svg";
+import user from "@/assets/icons/user-spy.svg";
 import { useLoginStore } from "@/stores/login";
 
 const loginStore = useLoginStore();
@@ -28,22 +28,17 @@ const loginStore = useLoginStore();
             <span>Victoria 8007 Australia</span>
           </div>
         </div>
-        <div class="header-row hidden">
-          <v-divider vertical thickness="2" />
-          <v-img :src="clock" height="30" width="30" />
-          <div class="header-column">
-            <span>Monday - Saturday - 8:00 - 18:00</span>
-            <span>Sunday - 8:00 - 14:00</span>
-          </div>
-          <v-divider vertical thickness="2" />
-        </div>
-        <div class="header-column">
+        <v-divider vertical thickness="2" class="hidden" />
+        <div class="header-row">
             <span v-if="loginStore.isAuthenticated" class="guest-name">
               {{ `Welcome, ${loginStore.user}` }}
             </span>
           <span v-else>
               Hi, Guest!
             </span>
+          <div class="user">
+            <v-img :src="user" class="user-icon" />
+          </div>
         </div>
       </div>
     </div>
